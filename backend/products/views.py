8e3b41a7c8990821e2752b2f36aeaafe748189c3
victor <victor@pangea.io>
2022-12-04 09:38:10 +0700

    add comment

--- backend/products/views.py
+++ backend/products/views.py
@@ -15,6 +15,7 @@ class ProductListCreateAPIView(
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
+        # email = serializer.validated_data.pop("email")
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
