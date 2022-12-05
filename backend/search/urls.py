5a718218cd0a79f06aa9a9f0b3ff74b25c97e269
victor <victor@pangea.io>
2022-12-05 14:56:10 +0700

    do not use algolia

--- backend/search/urls.py
+++ backend/search/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SearchListView.as_view(), name='search')
+    # path('', views.SearchListView.as_view(), name='search'),
+    path('', views.SearchListOldView.as_view(), name='search'),
 ]
